e9f385d73cb0705ca3b1383b1af8fe426eda4d11
MARCOS VINICIUS DA SILVA ROSA <rosa.3950@aluno.pr.senac.br>
2023-09-06 14:10:37 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/lista_exercicios_1/Lista_Exercicios_1.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_exercicios_1;
+
+import javax.xml.transform.sax.SAXSource;
+
+/**
+ *
+ * @author rosa.3950
+ */
+/*public class Lista_Exercicios_1 {
+
+    public static void main(String[] args) {
+        System.out.println( );   
+        int num1; 
+        int num2;
+        int num3;
+        int soma; 
+                
+     Scanner scanner = new Scanner ( System.in);
+        
+      
+       
+        System.out.println("informe seu o primeiro numero");
+        String nome;
+         
+        System.out.println("informe seu o segundo numero"); 
+        
+        int opção;
+        
+        float num1, num2, num3, resultado; 
+         
+        Scanner scanner = new Scanner (System.in)
+       
+        String nome;
+        
+        System.out.println("informe a operação");
+        System.out.println("1 - SOMA");
+        System.out.println("2 - SUBTRAÇÃO");       {
+        System.out.println("3 - MULTIPLICAÇÃO");
+        System.out.println("4 - DIVISÃO");
+        opção ""  scanner.nextInt();
+         
+        if (opção == 1) {//Soma
+        
+        nome = scanner.next(-----SOMA------);
+        System.out.println();"informe seu primeiro numero" 
+}
+*/
